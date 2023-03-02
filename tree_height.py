6249deb16f57efbfcf766d46f13d6f15d8824c74
VinetaBitte <124607776+VinetaBitte@users.noreply.github.com>
2023-03-02 19:13:46 +0000

    -

--- tree_height.py
+++ tree_height.py
@@ -6,13 +6,47 @@ import threading
 
 def compute_height(n, parents):
     # Write this function
+    tree = [[] for i in range(n)]
+    # izveidojam koku no listiem
+    for i in range(n):
+        if parents[i] == -1:
+            treeroot = i
+        tree[parents[i]].append(i)
+    # atrodam sakni
     max_height = 0
     # Your code here
+    #height = 1
+    stack = [(treeroot, 1)]
+    while len(stack) > 0:
+        mezgls, current_height = stack.pop()
+        if current_height > max_height:
+            max_height = current_height
+        for item in range(len(parents)):
+            if parents[item] == mezgls:
+                stack.append((item, current_height + 1))
+
+    
     return max_height
 
 
 def main():
     # implement input form keyboard and from files
+    text = input()
+    if "I" in text:
+        n = int(input())
+        parents = list(map(int, input().split()))
+        tree_height = compute_height(n, parents)
+    elif "F" in text:
+        filename = input()
+        if "a" in filename:
+            return
+        else:
+            with open("./test/" + filename, mode = "r") as fails:
+                n = int(fails.readline())
+                parents = list(map(int, fails.readline().split()))
+                tree_height = compute_height(n, parents) 
+
+    print(tree_height)
     
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
